30c5d1eb13cac32e73a617ba3edebb891d785060
kyoune01 <hahahakyoumonemutaina01@gmail.com>
2023-03-29 23:26:06 +0900

    [add] typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: 'gatsby-plugin-graphql-codegen',
+      options: {
+        fileName: `types/graphql-types.d.ts`
+      }
+    },
   ],
 }
 
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+require("ts-node").register({
+  compilerOptions: {
+    module: "commonjs",
+    target: "esnext",
+  },
+})
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
